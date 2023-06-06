b77533ca6f6c1b5c7c137d551402d8e8c587dc88
joaolucas3002 <92446800+joaolucas3002ht@users.noreply.github.com>
2023-06-06 16:44:26 -0300

    Updated Login Logic with react-router-dom 6.4 and Replaced UseAuthentication with Login
    Implemented a change in the login logic in the Login file to utilize the functionalities of react-router-dom 6.4. This update improves the login process by leveraging the latest features provided by the updated react-router-dom version. Additionally, replaced the UseAuthentication component with the Login functionality, resulting in a more streamlined and intuitive authentication flow.

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,66 +1,63 @@
+import { useEffect, useState } from 'react';
+import { Form, Link, useActionData, useLoaderData } from 'react-router-dom';
+import { login } from '../../hooks/useAuthentication';
 import styles from './index.module.css';
-import { useState, useEffect } from 'react';
-import { UseAuthentication } from '../../hooks/useAuthentication';
-const Index = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+
+export function Login() {
+  const data = useActionData();
+  const loading = useLoaderData();
+
   const [error, setError] = useState('');
 
-  const { login, error: authError, loading } = UseAuthentication();
-
-  const handleSubmit = async e => {
-    e.preventDefault();
-    setError('');
-    const user = {
-      email,
-      password,
-    };
-    const res = await login(user);
-    console.log(res);
-  };
   useEffect(() => {
-    if (authError) {
-      setError(authError);
-    }
-  }, [authError]);
+    console.log(loading);
+    setError(data?.error);
+  }, [loading]);
+
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
   return (
     <div className={styles.container}>
       <div className={styles.login}>
         <h1>Entrar</h1>
-        <form onSubmit={handleSubmit}>
+        {/* <form onSubmit={handleSubmit}> */}
+        <Form method='POST' action='/login'>
           <label>
             <span>E-mail:</span>
-            <input
-              type='email'
-              name='email'
-              required
-              placeholder='E-mail do usuário'
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-            />
+            <input type='email' name='email' required placeholder='E-mail do usuário' />
           </label>
           <label>
             <span>Senha:</span>
-            <input
-              type='password'
-              name='password'
-              required
-              placeholder='Senha do usuário'
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-            />
+            <input type='password' name='password' required placeholder='Senha do usuário' />
           </label>
-          {!loading && <button className='btn'>Entrar</button>}
-          {loading && (
-            <button className='btn' disabled>
-              Aguarde...
-            </button>
-          )}
+
+          <Link className='Link' to={'/register'} about='link para a pagina para criação de conta'>
+            Criar conta
+          </Link>
+
+          <button className='btn' disabled={loading}>
+            {!loading ? 'Entrar' : 'Aguarde...'}
+          </button>
+
           {error && <p className='error'>{error}</p>}
-        </form>
+        </Form>
       </div>
     </div>
   );
-};
+}
 
-export default Index;
+export async function actionLogin({ request }) {
+  const data = await request.formData();
+
+  const email = await data.get('email');
+
+  const password = await data.get('password');
+
+  const res = await login({ email, password });
+
+  console.log(res);
+
+  return res;
+}
