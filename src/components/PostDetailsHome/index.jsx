2934f2c2112c30dfcce7893cb74682187f952839
Benfi4rthur <arthur_benfica@hotmail.com>
2023-06-07 14:02:12 -0300

    imagens mandam para a pagina da postagem.

--- src/components/PostDetailsHome/index.jsx
+++ src/components/PostDetailsHome/index.jsx
@@ -6,12 +6,13 @@ const index = ({ post }) => {
 
   return (
     <div className={styles.post_detail}>
-      {mediaURL && (mediaURL.includes('.mp4') || mediaURL.includes('.webm')) ? (
-        <video src={mediaURL} alt={title}/>
-      ) : (
-        <img src={mediaURL} alt={title} />
-      )}
-
+      <Link to={`/posts/${id}`}>
+        {mediaURL && (mediaURL.includes('.mp4') || mediaURL.includes('.webm')) ? (
+          <video src={mediaURL} alt={title} />
+        ) : (
+          <img src={mediaURL} alt={title} />
+        )}
+      </Link>
       <h2>{title}</h2>
       <p className={styles.createdby}>{createdBy}</p>
       <div className={styles.tags}>
@@ -23,7 +24,7 @@ const index = ({ post }) => {
         ))}
       </div>
       <Link to={`/posts/${id}`} className='btn btn-outline'>
-        Ler
+        Ver mais...
       </Link>
       <hr></hr>
     </div>
